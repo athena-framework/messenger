10698ac75576b66e934e242873339ab73d3b9ac1
George Dietrich <george@dietrich.app>
2022-03-20 00:15:50 -0400

    Scaffold out basic middleware handling logic

--- src/athena-messenger.cr
+++ src/athena-messenger.cr
@@ -1,6 +1,44 @@
+require "./envelope"
+require "./message_interface"
+require "./message_bus"
+
+require "./middleware/*"
+require "./stamp/*"
+
 # Convenience alias to make referencing `Athena::Messenger` types easier.
 alias AMG = Athena::Messenger
 
 module Athena::Messenger
   VERSION = "0.1.0"
 end
+
+struct MyMessage
+  include AMG::MessageInterface
+end
+
+struct MyStamp < AMG::Stamp
+end
+
+class MyHandler
+  include AMG::Middleware::Interface
+
+  def handle(envelope : AMG::Envelope, stack : AMG::Middleware::StackInterface) : AMG::Envelope
+    pp "foo"
+
+    stack.next.handle envelope, stack
+  end
+end
+
+class MyHandler2
+  include AMG::Middleware::Interface
+
+  def handle(envelope : AMG::Envelope, stack : AMG::Middleware::StackInterface) : AMG::Envelope
+    pp "bar"
+
+    stack.next.handle envelope, stack
+  end
+end
+
+bus = AMG::MessageBus.new [MyHandler.new, MyHandler2.new] of AMG::Middleware::Interface
+
+bus.dispatch MyMessage.new
